a8718c2cd354a52be2539baed96e20450ba26f74
AlexandruNasalean <alexandru.nasalean@outlook.com>
2020-04-08 18:59:14 +0300

    Footer-Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { ContactPage } from "./ContactPage";
 
+
 function App() {
-  return <ContactPage />;
+  return <ContactPage /> ;
+
 }
 
 export default App;
--- src/ContactPage.js
+++ src/ContactPage.js
@@ -52,6 +52,7 @@ export function ContactPage() {
             Submit
           </Button>
         </Form>
+        
       </div>
     </div>
   );
