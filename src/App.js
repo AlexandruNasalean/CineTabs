775d573787650358790a2773eefd2c0b1f3837e7
adrianklivecz <adrian.clivet@yahoo.com>
2020-04-10 19:02:06 +0300

    organazing files

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
 import React, { Component } from "react";
-import { Header } from "./components/Header/Header"
-import { ContactPage } from "./ContactPage";
+import { Header } from "./components/Header/Header";
+import { ContactPage } from "./components/Pages/ContactPage/ContactPage";
 import { HomePage } from "./components/Pages/HomePage/HomePage";
 import { Genres } from "./components/Pages/Genres/Genres";
 import { AllMovies } from "./components/Pages/AllMovies/AllMovies";
-import { AdvancedSearch } from "./components/Pages/AdvancedSearch/AdvancedSearch"
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { AdvancedSearch } from "./components/Pages/AdvancedSearch/AdvancedSearch";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-class App extends Component  {
-  
+class App extends Component {
   render() {
-
     return (
-        <Router>
+      <Router>
         <div>
           <Header />
-          <Route exact path="/" component = {HomePage} /> 
-          <Route exact path="/AllMovies" component = {AllMovies} /> 
-          <Route exact path="/Genres" component = {Genres} /> 
-          <Route exact path="/AdvancedSearch" component = {AdvancedSearch} /> 
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/AllMovies" component={AllMovies} />
+          <Route exact path="/Genres" component={Genres} />
+          <Route exact path="/AdvancedSearch" component={AdvancedSearch} />
         </div>
-        </Router>
-
-    )
-  
-    }
-
+      </Router>
+    );
+  }
 }
 
 export default App;
