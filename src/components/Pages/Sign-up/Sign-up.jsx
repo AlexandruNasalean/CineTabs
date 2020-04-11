1fe156df9253ca1a10e47193237c655001892aed
AlexandruNasalean <alexandru.nasalean@outlook.com>
2020-04-11 16:04:11 +0300

    Update

--- src/components/Pages/Sign-up/Sign-up.jsx
+++ src/components/Pages/Sign-up/Sign-up.jsx
@@ -1,21 +1,29 @@
 import React, {Component} from "react";
 import { Card, ListGroup, ListGroupItem, Form, Button } from "react-bootstrap";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { fauserPlus } from '@fortawesome/free-solid-svg-icons'
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import "./SignUp.css";
+
 
 
 
 export class SignUp extends Component {
     render() {
         return (
-            <div class="title">
-                
+            <div>
+                <div class="title">
                 <h1> 
                 CREATE ACCOUNT
                 </h1>
-
-            </div>
+                <tr id="UserIcon"><FontAwesomeIcon icon={faUserPlus} /></tr>
+                </div>
+                <div>
+              
+                </div>
+               
            
+            </div>
+          
         )
     }
 }
