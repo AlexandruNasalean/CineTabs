310433efe11091d072deeba82e8d363c144477f5
AlexandruNasalean <alexandru.nasalean@outlook.com>
2020-04-11 15:27:17 +0300

    a

--- src/components/Pages/Sign-up/Sign-up.js
+++ src/components/Pages/Sign-up/Sign-up.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
 import { Card, ListGroup, ListGroupItem, Form, Button } from "react-bootstrap";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { fauserPlus } from '@fortawesome/free-solid-svg-icons'
 
 
 
@@ -7,6 +9,7 @@ export class SignUp extends Component {
     render() {
         return (
             <div class="title">
+                
                 <h1> 
                 CREATE ACCOUNT
                 </h1>
