b68585c32d234a2f6f1ebdb4d0fde3841d7be77a
AlexandruNasalean <alexandru.nasalean@outlook.com>
2020-04-17 14:27:31 +0300

    Update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,40 +7,16 @@ import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 export function Footer() {
   return (
-    <table>
-      <div class="footer">
-        <td>
-          <div class="description">
-            <h5>About the website</h5>
-            <tr>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-                eget enim eleifend, luctus nisi eget, pretium odio. Quisque id
-                tellus ex. Nunc pharetra molestie eleifend. Class aptent taciti
-                sociosqu ad litora torquent per conubia nostra, per inceptos
-                himenaeos.
-              </p>
-            </tr>
-          </div>
-        </td>
-        <td>
-          <h5>
-            <Link className="link" to="/Contact">
-              Contact
-            </Link>
-          </h5>
-          <div class="icoane">
-            <tr>
-              <FontAwesomeIcon icon={faEnvelope} />
-              <p>neptunTeam@scit.com</p>
-            </tr>
-            <tr>
-              <FontAwesomeIcon icon={faPhone} />
-              <p>+40 8978 5254 6484</p>
-            </tr>
-          </div>
-        </td>
-      </div>
-    </table>
+  
+         <div className="footer">
+         <p>About us</p>
+         <div className="">
+           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+             Integer consequat diam non fringilla feugiat. 
+             Vivamus sit amet viverra nibh. 
+             </p>
+         </div>
+         </div>
+         
   );
 }
