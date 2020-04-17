50f1279b224b77bfc62330e05b5226eb5b866227
AlexandruNasalean <alexandru.nasalean@outlook.com>
2020-04-17 17:33:38 +0300

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,19 +9,33 @@ export function Footer() {
   return (
   
          <div className="footer">
-           <divc className="footer-title">
-           <p>About us</p>
-           <p>Contact</p>
-           </divc>
-         <div className="footer-description">
-           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-             Integer consequat diam non fringilla feugiat. 
-             Vivamus sit amet viverra nibh. 
-             </p>
-             <p><FontAwesomeIcon icon={faEnvelope}/ ></p>
-             <p><FontAwesomeIcon icon={faPhone}/ ></p>
-         </div>
-         </div>
-         
-  );
-}
+           <div className="about-us-block">
+             <h5>About us</h5>
+             <div className="lorem">
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                eget enim eleifend, luctus nisi eget, pretium odio. Quisque id
+                tellus ex.<br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                eget enim eleifend, luctus nisi eget, <br />pretium odio.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                eget enim eleifend, luctus nisi eget, pretium odio.
+                </p>
+             </div>
+           </div>
+           <div className="contact-block">
+           <Link className="link" to="/Contact">
+              <h5>Contact</h5>
+            </Link>
+            <div className="contact-info">
+              <div className="contact-envelope">
+              <FontAwesomeIcon icon={faEnvelope} />
+              <p>neptunTeam@scit.com</p>
+              </div>
+              <div className="contact-phone">
+              <FontAwesomeIcon icon={faPhone} />
+              <p>+40 8978 5254 6484</p>
+              </div>
+            </div>
+           </div>
+              </div>
+             )
+             };
+
