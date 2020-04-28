4ae6dd7db4e8072faf34cc5a5a4aea69deb4d2a8
AlexandruNasalean <alexandru.nasalean@outlook.com>
2020-04-28 17:05:56 +0300

    Fetch and Search Button DOne

--- src/components/Pages/AdvancedSearch/AdvancedSearch.js
+++ src/components/Pages/AdvancedSearch/AdvancedSearch.js
@@ -1,14 +1,63 @@
 import React, {Component} from "react";
+import "./AdvSearch.css"
+
 
 
 export class AdvancedSearch extends Component {
-    render() {
-        return (
+    constructor(props) {
+        super(props);
+        this.state = {  
+            query: "",
+            data: [],
+            searchString: [],
+            emptySearch: false,
+        };
+    }
 
-            <h1> 
-               Advanced Search 
-            </h1>
-
+    handleInputChange =(event) =>{
+        this.setState(
+            {
+                query: event.target.value,
+            }
         )
     }
+
+    submitHandler = (e) =>{
+        e.preventDefault();
+        const searchQuery= this.state.query;
+        const url=`https://movies-app-siit.herokuapp.com/movies?Title=${searchQuery}`
+        fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+            this.setState({
+            searchString: json.results,
+            })
+        })
+    }
+    render() { 
+        console.log(this.state.searchString)
+
+        return ( 
+            <div className="container-lg">
+            <div className="Advanced-Filter">
+            <form className="col-lg-6 offset-lg-3" onSubmit={this.submitHandler}>
+            <div className="form-group">
+                    <div className="Title-SearchBar">
+                        <label id="Adv-Search-Title-Label">Title</label>
+                        <input 
+                        className="form-control"   
+                        type="text"
+                        id="filter"  
+                        placeholder="Search for a Title"
+                        onChange={this.handleInputChange} />
+                    </div>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+            </div>
+            <div className="Adv-Results"></div>
+        </div>
+         );
+    }
 }
+ 
