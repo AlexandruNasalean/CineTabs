1679490c6e991ad1b0d6e43c559810e9791aa5b2
AlexandruNasalean <alexandru.nasalean@outlook.com>
2020-05-02 14:44:48 +0300

    Added all the genres

--- src/components/Pages/AdvancedSearch/AdvancedSearch.js
+++ src/components/Pages/AdvancedSearch/AdvancedSearch.js
@@ -98,7 +98,7 @@ export class AdvancedSearch extends Component {
     return (
       <div className="container-lg">
         <div className="Advanced-Filter">
-          <form className="col-lg-6 offset-lg-3" onSubmit={this.submitHandler}>
+          <form className="col-lg-6 offset-lg-0" onSubmit={this.submitHandler}>
             <div className="form-group">
               <div className="Title-SearchBar">
                 <label id="Adv-Search-Title-Label">Title</label>
@@ -116,8 +116,21 @@ export class AdvancedSearch extends Component {
                               <Form.Check inline label="Comedy" name="Comedy" value="Comedy" onClick={this.CheckBoxChangeHandler}/>
                               <Form.Check inline label="Action" name="Action" value="Action" onClick={this.CheckBoxChangeHandler}/>
                               <Form.Check inline label="Adventure" name="Adventure" value="Adventure" onClick={this.CheckBoxChangeHandler}/>
-
-
+                              <Form.Check inline label="Family" name="Family" value="Family" onClick={this.CheckBoxChangeHandler}/>
+                              <Form.Check inline label="History" name="History" value="History" onClick={this.CheckBoxChangeHandler}/>
+                              <Form.Check inline label="Mystery" name="Mystery" value="Mystery" onClick={this.CheckBoxChangeHandler}/>
+                              <Form.Check inline label="Sci-Fi" name="Sci-Fi" value="Sci-Fi" onClick={this.CheckBoxChangeHandler}/>
+                              <Form.Check inline label="War" name="War" value="War" onClick={this.CheckBoxChangeHandler}/>
+                              <Form.Check inline label="Crime" name="Crime" value="Crime" onClick={this.CheckBoxChangeHandler}/>
+                              <Form.Check inline label="Fantasy" name="Fantasy" value="Fantasy" onClick={this.CheckBoxChangeHandler}/>
+                              <Form.Check inline label="Horror" name="Horror" value="Horror" onClick={this.CheckBoxChangeHandler}/>
+                              <Form.Check inline label="Sport" name="Sport" value="Sport" onClick={this.CheckBoxChangeHandler}/>
+                              <Form.Check inline label="Western" name="Western" value="Western" onClick={this.CheckBoxChangeHandler}/>
+                              <Form.Check inline label="Animation" name="Animation" value="Animation" onClick={this.CheckBoxChangeHandler}/>
+                              <Form.Check inline label="Documentary" name="Documentary" value="Documentary" onClick={this.CheckBoxChangeHandler}/>
+                              <Form.Check inline label="Drama	" name="Drama	" value="Drama	" onClick={this.CheckBoxChangeHandler}/>
+                              <Form.Check inline label="Romance" name="Romance" value="Romance" onClick={this.CheckBoxChangeHandler}/>
+                              <Form.Check inline label="Thriller" name="Thriller" value="Thriller" onClick={this.CheckBoxChangeHandler}/>
                           </div>
                           ))}
                     </div>
