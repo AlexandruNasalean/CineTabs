3e8242f1d74b7fa69476d19d3b99adf552f2a7fa
AlexandruNasalean <alexandru.nasalean@outlook.com>
2020-05-04 15:16:33 +0300

    "Update"

--- src/components/Pages/AdvancedSearch/AdvancedSearch.js
+++ src/components/Pages/AdvancedSearch/AdvancedSearch.js
@@ -4,6 +4,7 @@ import Cookies from "js-cookie";
 import { AdvancedSearchResult } from "./AdvancedSearchResults.js";
 import {Form} from "react-bootstrap";
 import {generateAdvancedSearchUrl} from "./AdvanceSearchUtils";
+// import {GenreFilter} from "./Filters/Genre"
 
 export class AdvancedSearch extends Component {
   constructor(props) {
@@ -89,6 +90,11 @@ export class AdvancedSearch extends Component {
       });
   };
 
+  // setGenreData = (Genres)=>(
+  //   this.setState({
+  //     Genre: Genres,
+  //   })
+  // )
   render() {
     // console.log(this.state.searchResults);
     const { emptySearch, searchResults, Genre} = this.state;
@@ -110,30 +116,31 @@ export class AdvancedSearch extends Component {
                   onChange={this.handleInputChange}
                 />
               </div>
-                    <div className="Genre-Filter">
-                          {['checkbox'].map((type) => (
-                          <div key={`inline-${type}`} className="mb-3">
-                              <Form.Check inline label="Comedy" name="Comedy" value="Comedy" onClick={this.CheckBoxChangeHandler}/>
-                              <Form.Check inline label="Action" name="Action" value="Action" onClick={this.CheckBoxChangeHandler}/>
-                              <Form.Check inline label="Adventure" name="Adventure" value="Adventure" onClick={this.CheckBoxChangeHandler}/>
-                              <Form.Check inline label="Family" name="Family" value="Family" onClick={this.CheckBoxChangeHandler}/>
-                              <Form.Check inline label="History" name="History" value="History" onClick={this.CheckBoxChangeHandler}/>
-                              <Form.Check inline label="Mystery" name="Mystery" value="Mystery" onClick={this.CheckBoxChangeHandler}/>
-                              <Form.Check inline label="Sci-Fi" name="Sci-Fi" value="Sci-Fi" onClick={this.CheckBoxChangeHandler}/>
-                              <Form.Check inline label="War" name="War" value="War" onClick={this.CheckBoxChangeHandler}/>
-                              <Form.Check inline label="Crime" name="Crime" value="Crime" onClick={this.CheckBoxChangeHandler}/>
-                              <Form.Check inline label="Fantasy" name="Fantasy" value="Fantasy" onClick={this.CheckBoxChangeHandler}/>
-                              <Form.Check inline label="Horror" name="Horror" value="Horror" onClick={this.CheckBoxChangeHandler}/>
-                              <Form.Check inline label="Sport" name="Sport" value="Sport" onClick={this.CheckBoxChangeHandler}/>
-                              <Form.Check inline label="Western" name="Western" value="Western" onClick={this.CheckBoxChangeHandler}/>
-                              <Form.Check inline label="Animation" name="Animation" value="Animation" onClick={this.CheckBoxChangeHandler}/>
-                              <Form.Check inline label="Documentary" name="Documentary" value="Documentary" onClick={this.CheckBoxChangeHandler}/>
-                              <Form.Check inline label="Drama	" name="Drama	" value="Drama	" onClick={this.CheckBoxChangeHandler}/>
-                              <Form.Check inline label="Romance" name="Romance" value="Romance" onClick={this.CheckBoxChangeHandler}/>
-                              <Form.Check inline label="Thriller" name="Thriller" value="Thriller" onClick={this.CheckBoxChangeHandler}/>
-                          </div>
-                          ))}
-                    </div>
+                   {/* <GenreFilter sendGenreData = {this.CheckBoxChangeHandler}></GenreFilter> */}
+                   <div className="Genre-Filter">
+      {['checkbox'].map((type) => (
+      <div key={`inline-${type}`} className="mb-3">
+          <Form.Check inline label="Comedy" name="Comedy" value="Comedy" onClick={this.CheckBoxChangeHandler}/>
+          <Form.Check inline label="Action" name="Action" value="Action" onClick={this.CheckBoxChangeHandler}/>
+          <Form.Check inline label="Adventure" name="Adventure" value="Adventure" onClick={this.CheckBoxChangeHandler}/>
+          <Form.Check inline label="Family" name="Family" value="Family" onClick={this.CheckBoxChangeHandler}/>
+          <Form.Check inline label="History" name="History" value="History" onClick={this.CheckBoxChangeHandler}/>
+          <Form.Check inline label="Mystery" name="Mystery" value="Mystery" onClick={this.CheckBoxChangeHandler}/>
+          <Form.Check inline label="Sci-Fi" name="Sci-Fi" value="Sci-Fi" onClick={this.CheckBoxChangeHandler}/>
+          <Form.Check inline label="War" name="War" value="War" onClick={this.CheckBoxChangeHandler}/>
+          <Form.Check inline label="Crime" name="Crime" value="Crime" onClick={this.CheckBoxChangeHandler}/>
+          <Form.Check inline label="Fantasy" name="Fantasy" value="Fantasy" onClick={this.CheckBoxChangeHandler}/>
+          <Form.Check inline label="Horror" name="Horror" value="Horror" onClick={this.CheckBoxChangeHandler}/>
+          <Form.Check inline label="Sport" name="Sport" value="Sport" onClick={this.CheckBoxChangeHandler}/>
+          <Form.Check inline label="Western" name="Western" value="Western" onClick={this.CheckBoxChangeHandler}/>
+          <Form.Check inline label="Animation" name="Animation" value="Animation" onClick={this.CheckBoxChangeHandler}/>
+          <Form.Check inline label="Documentary" name="Documentary" value="Documentary" onClick={this.CheckBoxChangeHandler}/>
+          <Form.Check inline label="Drama	" name="Drama	" value="Drama	" onClick={this.CheckBoxChangeHandler}/>
+          <Form.Check inline label="Romance" name="Romance" value="Romance" onClick={this.CheckBoxChangeHandler}/>
+          <Form.Check inline label="Thriller" name="Thriller" value="Thriller" onClick={this.CheckBoxChangeHandler}/>
+      </div>
+      ))}
+</div>
                   </div>
             <button type="submit" className="btn btn-primary">
               Submit
