5fc2cde66aa107ab32890f2d01f2c529d1d1b819
dianastetco <stetcod@yahoo.com>
2020-05-08 15:41:55 +0300

    runtimefilter

--- src/components/Pages/AdvancedSearch/AdvancedSearch.js
+++ src/components/Pages/AdvancedSearch/AdvancedSearch.js
@@ -2,8 +2,13 @@ import React, { Component } from "react";
 import "./AdvSearch.css";
 import Cookies from "js-cookie";
 import { AdvancedSearchResult } from "./AdvancedSearchResults.js";
+import { Form } from "react-bootstrap";
+import { generateAdvancedSearchUrl } from "./AdvanceSearchUtils";
+// import {GenreFilter} from "./Filters/Genre"
 import { RatingFilter } from "./searchFilters/RatingFilter";
 import { VotesFilter } from "./searchFilters/VotesFilter";
+import { CountryFilters } from "./searchFilters/CountryFilters";
+import { RuntimeFilter } from "./searchFilters/RuntimeFilter";
 
 export class AdvancedSearch extends Component {
   constructor(props) {
@@ -13,15 +18,16 @@ export class AdvancedSearch extends Component {
       data: [],
       searchResults: [],
       emptySearch: "",
+      Genre : [],
+      Country: [],
+      Runtime: [],
+      searchState: "",
+
     };
   }
-
   componentDidMount() {
-    const SerachQuerry = Cookies.get("SearchQuery");
-
-    if (SerachQuerry) {
-      const searchQuery = this.state.query;
-      const url = `https://movies-app-siit.herokuapp.com/movies?Title=${SerachQuerry}`;
+    const url = Cookies.get("CookieSearchQuery");
+    if (url) {
       fetch(url)
         .then((response) => response.json())
         .then((json) => {
@@ -36,7 +42,6 @@ export class AdvancedSearch extends Component {
             this.setState({
               emptySearch: false,
             });
-            Cookies.set("SearchQuery", searchQuery);
           }
         });
     }
@@ -47,42 +52,87 @@ export class AdvancedSearch extends Component {
       query: event.target.value,
     });
   };
+  checkCountryHandler = (event) =>{
+
+    console.log(event.target.name);
+    const Country = [...this.state.Country];
+
+    if(Country.includes(event.target.name)){
+      this.setState({
+        Country: Country.filter(element =>( element !== event.target.name))
+      })
+    }
+      else{
+      Country.push(event.target.name);
+      this.setState({
+        Country
+      })
+    }
+
+  }
+ 
+  CheckBoxChangeHandler = (event) => {
+    console.log(event.target.name);
+    const Genre = [...this.state.Genre];
+    if (Genre.includes(event.target.name)) {
+      this.setState({
+        Genre: Genre.filter((element) => element !== event.target.name),
+      });
+    } else {
+      Genre.push(event.target.name);
+      this.setState({
+        Genre,
+      });
+    }
+  };
+
+  CheckBoxChangeRuntimeHandler = (event) => {
+    console.log(event.target.name);
+    const Runtime = [...this.state.Runtime];
+    if (Runtime.includes(event.target.name)) {
+      this.setState({
+        Runtime: Runtime.filter((element) => element !== event.target.name),
+      });
+    } else {
+      Runtime.push(event.target.name);
+      this.setState({
+        Runtime,
+      });
+    }
+  };
 
   submitHandler = (e) => {
     e.preventDefault();
-    const searchQuery = this.state.query;
-    const url = `https://movies-app-siit.herokuapp.com/movies?Title=${searchQuery}`;
+    const url = generateAdvancedSearchUrl(this.state);
+    console.log(url);
+    var _ =  require  ('lodash');
     fetch(url)
       .then((response) => response.json())
       .then((json) => {
         this.setState({
           searchResults: json.results,
         });
+        Cookies.set("CookieSearchQuery", url);
         if (json.results.length === 0) {
           this.setState({
             emptySearch: true,
+            searchState: false,
           });
         } else {
           this.setState({
             emptySearch: false,
+            searchState: true,
           });
-          Cookies.set("SearchQuery", searchQuery);
         }
       });
   };
 
+
+
   filterByRating(minRating, maxRating) {
     this.setState({
-      searchResults: this.state.searchResults.filter((movie) => {
-        return movie.sort((a, b) => {
-          const minRating = Number(a.imdbRating);
-          const maxRating = Number(b.imdbRating);
-
-          return maxRating - minRating;
-        });
-      }),
+      searchResults: this.state.searchResults.filter((movie) => {}),
     });
-    console.log(minRating, maxRating);
   }
 
   filterByVotes(minVotes, maxVotes) {
@@ -91,14 +141,22 @@ export class AdvancedSearch extends Component {
     });
   }
 
+  filterByRuntime(minRuntime, maxRuntime){
+    this.setState({
+      searchResults: this.state.searchResults.filter((movie) => {}),
+    });
+      }
+      
+
   render() {
-    const { emptySearch, searchResults } = this.state;
+    const { emptySearch, searchResults, searchState} = this.state;
     console.log(searchResults);
+    var _ =  require  ('lodash');
 
     return (
       <div className="container-lg">
         <div className="Advanced-Filter">
-          <form className="col-lg-6 offset-lg-3" onSubmit={this.submitHandler}>
+          <form className="col-lg-6 offset-lg-0" onSubmit={this.submitHandler}>
             <div className="form-group">
               <div className="Title-SearchBar">
                 <label id="Adv-Search-Title-Label">Title</label>
@@ -110,15 +168,162 @@ export class AdvancedSearch extends Component {
                   onChange={this.handleInputChange}
                 />
               </div>
-              <RatingFilter
-                searchResults={searchResults}
-                filterByRating={this.filterByRating}
-              />
-              <VotesFilter
-                searchResults={searchResults}
-                filterByVotes={this.filterByVotes}
-              />
-            </div>
+              {/* <GenreFilter sendGenreData = {this.CheckBoxChangeHandler}></GenreFilter> */}
+              <div className="Genre-Filter">
+                {["checkbox"].map((type) => (
+                  <div key={`inline-${type}`} className="mb-3">
+                    <Form.Check
+                      inline
+                      label="Comedy"
+                      name="Comedy"
+                      value="Comedy"
+                      onClick={this.CheckBoxChangeHandler}
+                    />
+                    <Form.Check
+                      inline
+                      label="Action"
+                      name="Action"
+                      value="Action"
+                      onClick={this.CheckBoxChangeHandler}
+                    />
+                    <Form.Check
+                      inline
+                      label="Adventure"
+                      name="Adventure"
+                      value="Adventure"
+                      onClick={this.CheckBoxChangeHandler}
+                    />
+                    <Form.Check
+                      inline
+                      label="Family"
+                      name="Family"
+                      value="Family"
+                      onClick={this.CheckBoxChangeHandler}
+                    />
+                    <Form.Check
+                      inline
+                      label="History"
+                      name="History"
+                      value="History"
+                      onClick={this.CheckBoxChangeHandler}
+                    />
+                    <Form.Check
+                      inline
+                      label="Mystery"
+                      name="Mystery"
+                      value="Mystery"
+                      onClick={this.CheckBoxChangeHandler}
+                    />
+                    <Form.Check
+                      inline
+                      label="Sci-Fi"
+                      name="Sci-Fi"
+                      value="Sci-Fi"
+                      onClick={this.CheckBoxChangeHandler}
+                    />
+                    <Form.Check
+                      inline
+                      label="War"
+                      name="War"
+                      value="War"
+                      onClick={this.CheckBoxChangeHandler}
+                    />
+                    <Form.Check
+                      inline
+                      label="Crime"
+                      name="Crime"
+                      value="Crime"
+                      onClick={this.CheckBoxChangeHandler}
+                    />
+                    <Form.Check
+                      inline
+                      label="Fantasy"
+                      name="Fantasy"
+                      value="Fantasy"
+                      onClick={this.CheckBoxChangeHandler}
+                    />
+                    <Form.Check
+                      inline
+                      label="Horror"
+                      name="Horror"
+                      value="Horror"
+                      onClick={this.CheckBoxChangeHandler}
+                    />
+                    <Form.Check
+                      inline
+                      label="Sport"
+                      name="Sport"
+                      value="Sport"
+                      onClick={this.CheckBoxChangeHandler}
+                    />
+                    <Form.Check
+                      inline
+                      label="Western"
+                      name="Western"
+                      value="Western"
+                      onClick={this.CheckBoxChangeHandler}
+                    />
+                    <Form.Check
+                      inline
+                      label="Animation"
+                      name="Animation"
+                      value="Animation"
+                      onClick={this.CheckBoxChangeHandler}
+                    />
+                    <Form.Check
+                      inline
+                      label="Documentary"
+                      name="Documentary"
+                      value="Documentary"
+                      onClick={this.CheckBoxChangeHandler}
+                    />
+                    <Form.Check
+                      inline
+                      label="Drama	"
+                      name="Drama	"
+                      value="Drama	"
+                      onClick={this.CheckBoxChangeHandler}
+                    />
+                    <Form.Check
+                      inline
+                      label="Romance"
+                      name="Romance"
+                      value="Romance"
+                      onClick={this.CheckBoxChangeHandler}
+                    />
+                    <Form.Check
+                      inline
+                      label="Thriller"
+                      name="Thriller"
+                      value="Thriller"
+                      onClick={this.CheckBoxChangeHandler}
+                    />
+                  </div>
+                ))}
+                    </div>
+                    { searchState ?( 
+                      <React.Fragment>
+                       <CountryFilters searchResults={searchResults}/>
+                       <RatingFilter
+                         searchResults={searchResults}
+                         filterByRating={this.filterByRating}
+                       />
+                       <VotesFilter
+                         searchResults={searchResults}
+                         filterByVotes={this.filterByVotes}
+                       />
+                       <RuntimeFilter
+                       searchResults={searchResults}
+                       filterByRuntime={this.filterByRuntime}
+                       />
+                       </React.Fragment>
+                    ) : (
+                      ""
+                    )
+                     }
+                    
+                  </div>
+
             <button type="submit" className="btn btn-primary">
               Submit
             </button>
