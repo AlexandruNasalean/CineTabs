9026eeca9712710c9c18967b199ccd6efd9456d0
adrianklivecz <adrian.clivet@yahoo.com>
2020-05-09 21:44:00 +0300

    add-movie button in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,17 +9,16 @@ export class Header extends Component {
 
     return (
       <div className="header">
-        <li>
-          <Link className="link-homepage" to="/">
-            <img
-              className="logo"
-              src="https://i.imgur.com/r3dLpmN.png"
-              alt="Smiley face"
-            />
-          </Link>
-        </li>
-
         <ul className="menu-ul-list">
+          <li>
+            <Link className="link-homepage" to="/">
+              <img
+                className="logo"
+                src="https://i.imgur.com/r3dLpmN.png"
+                alt="Smiley face"
+              />
+            </Link>
+          </li>
           <li>
             <Link className="link-header" to="/AllMovies">
               All Movies
@@ -35,26 +34,33 @@ export class Header extends Component {
               Advanced Search
             </Link>
           </li>
-          {isLoggedIn ? (
-            <React.Fragment>
-            <li className="username">{username}</li>
-            <Button onClick={onShowLogOutModal}>Log Out</Button>
-            </React.Fragment>
-          ) : (
-            <React.Fragment>
-              <li>
-                <Link className="login" to="/login">
-                  Login
-                </Link>
-              </li>
-              <li>
-                <Link className="sign-up" to="/sign-up">
-                  Sign-up
-                </Link>
-              </li>
-            </React.Fragment>
-          )}
         </ul>
+        <div>
+          <ul className="on-login-buttons">
+            {isLoggedIn ? (
+              <React.Fragment>
+                <li>
+                  <Link to="/addmovie">Add Movie</Link>
+                </li>
+                <li className="username">{username}</li>
+                <Button onClick={onShowLogOutModal}>Log Out</Button>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                <li>
+                  <Link className="login" to="/login">
+                    Login
+                  </Link>
+                </li>
+                <li>
+                  <Link className="sign-up" to="/sign-up">
+                    Sign-up
+                  </Link>
+                </li>
+              </React.Fragment>
+            )}
+          </ul>
+        </div>
       </div>
     );
   }
