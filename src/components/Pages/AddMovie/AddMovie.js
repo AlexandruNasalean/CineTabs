fcc52e9c14eefd73a7bf068cae3600d1492af901
adrianklivecz <adrian.clivet@yahoo.com>
2020-05-11 23:01:54 +0300

    creating new files

---
+++ src/components/Pages/AddMovie/AddMovie.js
@@ -0,0 +1,23 @@
+fetch("https://movies-api-siit.herokuapp.com/auth/login", {
+  method: "POST",
+  path: "/movies",
+  mode: "cors",
+  cache: "no-cache",
+  credentials: "same-origin",
+  headers: {
+    "Content-Type": "application/json",
+    "X-Auth-Token": token,
+  },
+  redirect: "follow",
+  referrerPolicy: "no-referrer",
+  body: JSON.stringify({
+    username: "stefan_cel_mare",
+    password: "1234",
+  }),
+})
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json);
+
+    document.cookie = `token=${json.accessToken}`;
+  });
