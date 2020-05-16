71e9de4d8bf80ba51b0a27577928c2724fd2b9d0
dianastetco <stetcod@yahoo.com>
2020-05-17 00:06:41 +0300

    done

--- src/components/Pages/AdvancedSearch/searchFilters/RuntimeFilter.js
+++ src/components/Pages/AdvancedSearch/searchFilters/RuntimeFilter.js
@@ -1,40 +1,25 @@
 import React, { Component } from "react";
+import { uniqBy } from "lodash";
 import "../AdvSearch.css";
-// import { uniq } from "lodash";
+
 
 export class RuntimeFilter extends Component {
-  state = {
-    minRuntime: null,
-    maxRuntime: null,
-  };
-
-  handleOnChange = (event) => {
-    const {minRuntime, maxRuntime} = this.state;
-    this.props.filterByRuntime(minRuntime, maxRuntime);
-console.log(event.target.value);  
-  };
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
 
   render() {
-    const { searchResults } = this.props;
-
+    const { RuntimeHandler, searchResults } =this.props;
+    const RuntimeFiltered = uniqBy(searchResults, 'Runtime')
+    console.log(RuntimeFiltered)
     return (
       <div className="runtime-filter">
         <label>Runtime</label>
         <div className="movie-runtime">
-          <p>From</p>
-          <select className="runtime-dropdown" name="movie-runtime-drop-down">
-            {searchResults.map((movie, index) => (
-              <option key={index} value={movie.imdbRuntime}>
-                {movie.imdbRuntime}
-              </option>
-            ))}
-          </select>
-          <p>to</p>
-          <select className="runtime-dropdown" name="movie-runtime-drop-down">
-            {searchResults.map((movie, index) => (
-              <option key={index} value={movie.imdbRuntime}>
-                {movie.imdbRuntime}
-              </option>
+          <select className="runtime-dropdown" onChange={RuntimeHandler}>
+            {RuntimeFiltered?.map((movie, index) => (
+              <option key={index} value={movie.Runtime}>{movie.Runtime}</option>
             ))}
           </select>
         </div>
